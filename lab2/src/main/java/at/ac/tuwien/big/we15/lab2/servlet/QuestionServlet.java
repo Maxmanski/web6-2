ebee5415fa79f6f446099f528edda6a98b5d4775
Nicole Haberberger <nicole.haberberger@gmail.com>
2015-04-24 21:33:08 +0200

    calculate price

--- lab2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/QuestionServlet.java
+++ lab2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/QuestionServlet.java
@@ -53,20 +53,31 @@ public class QuestionServlet extends HttpServlet {
 		
 		String selectedValue=request.getParameter("question_selection");	
         if(!selectedValue.equals("")){
+        	int price =0;
         	try{
         		int selectedQuestionId = Integer.parseInt(selectedValue);
         		for(Category cat : categories){
+        			int index=1;
         			for(Question q : cat.getQuestions()){
         				if(q.getId() == selectedQuestionId){
+        					//calculate price
+        					switch(index){
+	        					case 1: price = 100; break;
+	        					case 2: price = 200; break;
+	        					case 3: price = 500; break;
+	        					case 4: price = 750; break;
+	        					case 5: price = 1000; break;
+        					}
+        					q.setAnswered(true);
         					session.setAttribute("currentQuestion", q);
         					session.setAttribute("counter", ++counter);
-        					session.setAttribute("price", 0);
-        					q.setAnswered(true);
- 
+        					session.setAttribute("price", price);
+        					
         					RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/question.jsp"); 
         					dispatcher.forward(request, response);
         					return;
         				}
+            			index++;
         			}
         		}
         	}catch(Exception e){
@@ -74,5 +85,6 @@ public class QuestionServlet extends HttpServlet {
         	}
         }
 	}
+	
 
 }
