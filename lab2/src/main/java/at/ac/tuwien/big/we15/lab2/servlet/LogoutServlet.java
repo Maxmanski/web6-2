fbd13da214c8f0ecd35c0cb2e2e9f302ef59c967
Max <maxmanski@live.at>
2015-04-26 21:40:12 +0200

    LogoutServlet

---
+++ lab2/src/main/java/at/ac/tuwien/big/we15/lab2/servlet/LogoutServlet.java
@@ -0,0 +1,35 @@
+package at.ac.tuwien.big.we15.lab2.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ *
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// invalidate the session and redirect to login.jsp
+		HttpSession session = req.getSession();
+		if(session != null){
+			session.invalidate();
+		}
+		getServletContext().getRequestDispatcher("/login.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
